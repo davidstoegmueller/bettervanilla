42b4b033230bbd064b02bb81b6a43dbc3f4b2730
David Stögmüller <david.stg@gmx.at>
2025-07-05 11:12:17 +0200

    Limit farmland protection to planted crops

--- src/main/java/com/daveestar/bettervanilla/Main.java
+++ src/main/java/com/daveestar/bettervanilla/Main.java
@@ -23,6 +23,7 @@ import com.daveestar.bettervanilla.events.PreventDimension;
 import com.daveestar.bettervanilla.events.ServerMOTD;
 import com.daveestar.bettervanilla.events.SittableStairs;
 import com.daveestar.bettervanilla.events.SleepingRain;
+import com.daveestar.bettervanilla.events.CropProtection;
 import com.daveestar.bettervanilla.manager.AFKManager;
 import com.daveestar.bettervanilla.manager.CompassManager;
 import com.daveestar.bettervanilla.manager.DeathPointsManager;
@@ -109,6 +110,7 @@ public class Main extends JavaPlugin {
     manager.registerEvents(new SittableStairs(), this);
     manager.registerEvents(new PreventDimension(), this);
     manager.registerEvents(new SleepingRain(), this);
+    manager.registerEvents(new CropProtection(), this);
   }
 
   @Override
